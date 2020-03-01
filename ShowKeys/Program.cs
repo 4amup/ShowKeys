6d58bf72b5dd465c9fbb55776fac696ed510a062
lixiaochun <490398311@qq.com>
2020-03-01 22:14:25 +0800

    update gitignore

--- ShowKeys/Program.cs
+++ ShowKeys/Program.cs
@@ -6,7 +6,7 @@ namespace ShowKeys
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#!");
         }
     }
 }
